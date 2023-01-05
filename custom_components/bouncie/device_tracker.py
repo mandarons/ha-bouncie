cd73977fb75fbf3f3fa18101c4968f1510433ccf
Mandar Patil <mandarons@pm.me>
2023-01-05 17:54:13 +0000

    Fix for device_tracker not updating automatically

--- custom_components/bouncie/device_tracker.py
+++ custom_components/bouncie/device_tracker.py
@@ -2,11 +2,12 @@
 
 from homeassistant.components.device_tracker import SOURCE_TYPE_GPS
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import slugify
 from homeassistant.helpers.entity import DeviceInfo
 
-from . import const
+from . import const, BouncieDataUpdateCoordinator
 
 ATTRIBUTION = "Data provided by Bouncie"
 
@@ -16,29 +17,27 @@ async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entitie
     coordinator = hass.data[const.DOMAIN][config_entry.entry_id]
     async_add_entities(
         [
-            BouncieVehicleTracker(vehicle_info)
+            BouncieVehicleTracker(coordinator, vehicle_info)
             for vehicle_info in coordinator.data["vehicles"]
-        ]
+        ],
+        True,
     )
 
 
-class BouncieVehicleTracker(TrackerEntity):
+class BouncieVehicleTracker(
+    CoordinatorEntity[BouncieDataUpdateCoordinator], TrackerEntity
+):
     """Representation of a Tesla car location device tracker."""
 
     _attr_attribution = ATTRIBUTION
 
     def __init__(
         self,
+        coordinator: BouncieDataUpdateCoordinator,
         vehicle_info: dict,
     ) -> None:
         """Initialize car location entity."""
         self._vehicle_info = vehicle_info
-        self._fuel_level = vehicle_info["stats"]["fuelLevel"]
-        self._latitude = vehicle_info["stats"]["location"]["lat"]
-        self._longitude = vehicle_info["stats"]["location"]["lon"]
-        self._heading = vehicle_info["stats"]["location"]["heading"]
-        self._name = vehicle_info["stats"]["location"]["address"]
-        self._speed = vehicle_info["stats"]["speed"]
         self._attr_has_entity_name = True
         self._attr_unique_id = slugify(f'{self._vehicle_info["nickName"]} tracker')
         self._attr_device_info = DeviceInfo(
@@ -48,6 +47,7 @@ class BouncieVehicleTracker(TrackerEntity):
             name=self._vehicle_info["nickName"],
             hw_version=self._vehicle_info[const.VEHICLE_MODEL_KEY]["year"],
         )
+        super().__init__(coordinator)
 
     @property
     def source_type(self):
@@ -57,22 +57,35 @@ class BouncieVehicleTracker(TrackerEntity):
     @property
     def longitude(self):
         """Return longitude."""
-        return self._longitude
+        return self._vehicle_info["stats"]["location"]["lon"]
 
     @property
     def latitude(self):
         """Return latitude."""
-        return self._latitude
+        return self._vehicle_info["stats"]["location"]["lat"]
 
     @property
     def extra_state_attributes(self):
         """Return device state attributes."""
         return {
-            "heading": self._heading,
-            "speed": self._speed,
+            "heading": self._vehicle_info["stats"]["location"]["heading"],
+            "speed": self._vehicle_info["stats"]["speed"],
         }
 
     @property
     def force_update(self):
         """Disable forced updated since we are polling via the coordinator updates."""
         return False
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        self._vehicle_info = (
+            lambda: [
+                vehicle
+                for vehicle in self.coordinator.data["vehicles"]
+                if vehicle["vin"] == self._vehicle_info["vin"]
+            ][0]
+            or self._vehicle_info
+        )()
+        self.async_write_ha_state()
+        return super()._handle_coordinator_update()
