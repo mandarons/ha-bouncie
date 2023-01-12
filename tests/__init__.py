1e7871b6492b74ebafc2c27ef375ab9e547a6003
Mandar Patil <mandarons@pm.me>
2023-01-12 17:34:18 +0000

    Patch for missing battery info

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,14 +10,18 @@ from custom_components.bouncie.coordinator import AsyncRESTAPIClient
 from . import const
 
 
-def setup_mock_controller(mock_controller):
+def setup_mock_controller(mock_controller, mock_vehicles_response):
     """Set up mock controller with mock data."""
     instance = mock_controller.return_value
     instance.get_access_token.return_value = True
-    instance.get_all_vehicles.return_value = const.MOCK_VEHICLES_RESPONSE
+    instance.get_all_vehicles.return_value = mock_vehicles_response
 
 
-async def setup_platform(hass: HomeAssistant, platform: str) -> MockConfigEntry:
+async def setup_platform(
+    hass: HomeAssistant,
+    platform: str,
+    mock_vehicles_response=const.MOCK_VEHICLES_RESPONSE,
+) -> MockConfigEntry:
     """Set up Bouncie platform."""
     mock_entry = MockConfigEntry(
         domain=const.DOMAIN, title="Bouncie", data=const.MOCK_CONFIG_ENTRY, options=None
@@ -28,7 +32,7 @@ async def setup_platform(hass: HomeAssistant, platform: str) -> MockConfigEntry:
         "custom_components.bouncie.coordinator.AsyncRESTAPIClient",
         spec=AsyncRESTAPIClient,
     ) as mock_controller:
-        setup_mock_controller(mock_controller)
+        setup_mock_controller(mock_controller, mock_vehicles_response)
         assert await async_setup_component(hass, const.DOMAIN, {})
     await hass.async_block_till_done()
 
