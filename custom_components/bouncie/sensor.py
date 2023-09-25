5ac8fee75210ca2686a2b6d8976222cce8a0c14e
mandarons <mandarons@pm.me>
2023-09-25 14:29:16 +0000

    Fix for warnings

--- custom_components/bouncie/sensor.py
+++ custom_components/bouncie/sensor.py
@@ -95,6 +95,7 @@ SENSORS: tuple[BouncieSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SPEED,
         suggested_unit_of_measurement="mph",
+        native_unit_of_measurement="mph",
         value_fn=lambda vehicle_info: int(vehicle_info["stats"]["speed"]),
         extra_attrs_fn=update_car_stats_attributes,
     ),
